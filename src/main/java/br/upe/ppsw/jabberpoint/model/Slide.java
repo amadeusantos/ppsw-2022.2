4fb87bd3c4454357eb172de46940018aa21216d8
amadeusantos <amadeusantos31@gmail.com>
2023-02-17 17:24:46 -0300

    retirando a draw do model Slide

--- src/main/java/br/upe/ppsw/jabberpoint/model/Slide.java
+++ src/main/java/br/upe/ppsw/jabberpoint/model/Slide.java
@@ -1,8 +1,5 @@
 package br.upe.ppsw.jabberpoint.model;
 
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.image.ImageObserver;
 import java.util.Vector;
 
 public class Slide {
@@ -44,30 +41,8 @@ public class Slide {
   public int getSize() {
     return items.size();
   }
-
-  public void draw(Graphics g, Rectangle area, ImageObserver view) {
-    float scale = getScale(area);
-
-    int y = area.y;
-
-    SlideItem slideItem = this.title;
-    Style style = Style.getStyle(slideItem.getLevel());
-    slideItem.draw(area.x, y, scale, g, style, view);
-
-    y += slideItem.getBoundingBox(g, view, scale, style).height;
-
-    for (int number = 0; number < getSize(); number++) {
-      slideItem = (SlideItem) getSlideItems().elementAt(number);
-
-      style = Style.getStyle(slideItem.getLevel());
-      slideItem.draw(area.x, y, scale, g, style, view);
-
-      y += slideItem.getBoundingBox(g, view, scale, style).height;
-    }
-  }
-
-  private float getScale(Rectangle area) {
-    return Math.min(((float) area.width) / ((float) WIDTH),
-        ((float) area.height) / ((float) HEIGHT));
+  
+  public TextItem getTextItemTitle() {
+	  return this.title;
   }
 }
--- src/main/java/br/upe/ppsw/jabberpoint/view/SlideViewerComponent.java
+++ src/main/java/br/upe/ppsw/jabberpoint/view/SlideViewerComponent.java
@@ -24,6 +24,8 @@ public class SlideViewerComponent extends JComponent {
   private static final int FONTHEIGHT = 10;
   private static final int XPOS = 1100;
   private static final int YPOS = 20;
+  public final static int WIDTH = 1200;
+  public final static int HEIGHT = 800;
 
   private Slide slide;
   private Font labelFont = null;
@@ -68,7 +70,34 @@ public class SlideViewerComponent extends JComponent {
 
     Rectangle area = new Rectangle(0, YPOS, getWidth(), (getHeight() - YPOS));
 
-    slide.draw(g, area, this);
+    draw(g, area, this);
   }
+  
+  public void draw(Graphics g, Rectangle area, ImageObserver view) {
+	    float scale = getScale(area);
 
+	    int y = area.y;
+
+	    SlideItem slideItem = slide.getTextItemTitle();
+	    Style style = Style.getStyle(slideItem.getLevel());
+	    slideItem.draw(area.x, y, scale, g, style, view);
+
+	    y += slideItem.getBoundingBox(g, view, scale, style).height;
+
+	    for (int number = 0; number < slide.getSize(); number++) {
+	      slideItem = (SlideItem) slide.getSlideItems().elementAt(number);
+
+	      style = Style.getStyle(slideItem.getLevel());
+	      slideItem.draw(area.x, y, scale, g, style, view);
+
+	      y += slideItem.getBoundingBox(g, view, scale, style).height;
+	    }
+	  }
+
+	  private float getScale(Rectangle area) {
+	    return Math.min(((float) area.width) / ((float) WIDTH),
+	        ((float) area.height) / ((float) HEIGHT));
+	  }
+
+		 
 }
